24900439ad19a985416e25f771e18f18f62cec25
yujiseok <koesijuy@urbanisom.com>
2022-03-14 18:38:46 +0900

    주석

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 const round = document.querySelector(".round-wrap");
-// const btn = document.querySelector(".toggle");
-// let rotate = false;
-let runner;
 let degrees = 0;
 const itemLength = 10;
 const gap = 10;
@@ -18,19 +15,23 @@ function circle(wr, hr, radian) {
 
 function display(cx, cy, wr, hr) {
   for (let i = 0; i < itemLength; i++) {
-    const degree = (360 / itemLength) * i + increase;
+    const degree = (360 / itemLength) * i + increase; // 360 / 10 * 반복문 i + increase
     const radian = (degree * Math.PI) / 180;
-    const point = circle(wr, hr, radian);
-    const x = Math.floor(point.x + cx);
-    const y = Math.floor(point.y + cy);
+    const point = circle(wr, hr, radian); // 265, 265, radian
+    // console.log("degree", degree);
+    // console.log("radian", radian);
+    const x = Math.floor(point.x + cx); // point.x = 265 * Math.cos(Radian), cx = 265
+    const y = Math.floor(point.y + cy); // point.y = 265 * Math.sin(Radian), cy = 265
     const el = document.querySelector(`.point${i}`);
     el.style.left = x - gap + "px";
     el.style.top = y - gap + "px";
   }
 }
 
+display(265, 265, 265, 265);
+
 function rotate() {
-  runner = setInterval(function () {
+  setInterval(function () {
     increase++;
     console.log(increase);
     display(265, 265, 265, 265);
@@ -40,17 +41,3 @@ function rotate() {
 }
 
 rotate();
-
-// function stop() {
-//   clearInterval(runner);
-// }
-
-// btn.addEventListener("click", function () {
-//   if (!rotate) {
-//     rotate = true;
-//     start();
-//   } else {
-//     rotate = false;
-//     stop();
-//   }
-// });
